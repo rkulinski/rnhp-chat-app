8b6f9654a1cf310916ae2b9cbb41719f90850e63
kulinskir <robert.kulinski93@gmail.com>
2018-10-19 22:17:52 +0200

    Shake Animation

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -3,8 +3,15 @@
  * @format
  * @flow
  */
-import React from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, Image } from 'react-native'
+import React, { Component } from 'react'
+import {
+  Animated,
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Image,
+} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { likeMessage } from '../../utils/api'
 import { UserContext } from '../../screens/chat/user-context'
@@ -41,37 +48,78 @@ type Props = {
   likes_count: number,
 }
 
-const MessageElementComponent = ({
-  id,
-  image,
-  content,
-  author,
-  likes_count: likesCount,
-}: Props) => (
-  <View style={styles.messagesContainer}>
-    {image && (
-      <Image source={{ uri: image }} style={{ width: 100, height: 100 }} />
-    )}
-    <Text>{content}</Text>
-    <View style={styles.captionContainer}>
-      <Text style={styles.author}>{author}</Text>
-      <View style={styles.likeContainer}>
-        <UserContext.Consumer>
-          {username => (
-            <TouchableOpacity
-              style={styles.likeButton}
-              onPress={() => {
-                likeMessage(id, username)
-              }}
-            >
-              <Icon name="ios-thumbs-up" size={35} />
-            </TouchableOpacity>
+class MessageElementComponent extends Component<Props> {
+  _defaultTransition = 250
+
+  state = {
+    _translateXValue: new Animated.Value(0),
+  }
+
+  componentDidMount() {
+    this.shakeAnimation()
+  }
+
+  shakeAnimation() {
+    const { _translateXValue } = this.state
+
+    const { timing, sequence } = Animated
+
+      sequence([
+        timing(_translateXValue, {
+          toValue: 25,
+          duration: this._defaultTransition,
+        }),
+        timing(_translateXValue, {
+          toValue: -25,
+          duration: this._defaultTransition,
+        }),
+        timing(_translateXValue, {
+          toValue: 0,
+          duration: this._defaultTransition,
+        }),
+      ])
+    .start()
+  }
+
+  render() {
+    const { id, image, content, author, likes_count: likesCount } = this.props
+
+    return (
+      <Animated.View
+        style={{
+          transform: [{ translateX: this.state._translateXValue }],
+        }}
+      >
+        <View style={styles.messagesContainer}>
+          {image && (
+            <Image
+              source={{ uri: image }}
+              style={{ width: 100, height: 100 }}
+            />
           )}
-        </UserContext.Consumer>
-        <Text>{likesCount}</Text>
-      </View>
-    </View>
-  </View>
-)
+          <Text>{content}</Text>
+          <View style={styles.captionContainer}>
+            <Text style={styles.author}>{author}</Text>
+            <View style={styles.likeContainer}>
+              <UserContext.Consumer>
+                {username => (
+                  <TouchableOpacity
+                    style={styles.likeButton}
+                    onPress={() => {
+                      likeMessage(id, username)
+                    }}
+                  >
+                    <Icon name="ios-thumbs-up" size={35} />
+                  </TouchableOpacity>
+                )}
+              </UserContext.Consumer>
+              <Text>{likesCount}</Text>
+            </View>
+          </View>
+        </View>
+      </Animated.View>
+    )
+  }
+}
 
 export default MessageElementComponent
